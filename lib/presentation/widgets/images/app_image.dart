9c81cb2ba010dca18400df893710715446f6fb99
djy <djybeyond@163.com>
2025-12-18 23:33:27 +0800

    add db

---
+++ lib/presentation/widgets/images/app_image.dart
@@ -0,0 +1,48 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class AppImage extends StatelessWidget {
+  const AppImage({
+    super.key,
+    required this.url,
+    this.width,
+    this.height,
+    this.fit = BoxFit.cover,
+    this.borderRadius,
+    this.placeholder,
+    this.errorWidget,
+  });
+
+  final String url;
+  final double? width;
+  final double? height;
+  final BoxFit fit;
+  final BorderRadius? borderRadius;
+  final Widget? placeholder;
+  final Widget? errorWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    final image = CachedNetworkImage(
+      imageUrl: url,
+      width: width,
+      height: height,
+      fit: fit,
+      placeholder: (_, __) =>
+          placeholder ??
+          const Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      errorWidget: (_, __, ___) =>
+          errorWidget ??
+          const Icon(Icons.broken_image),
+    );
+
+    if (borderRadius != null) {
+      return ClipRRect(
+        borderRadius: borderRadius!,
+        child: image,
+      );
+    }
+
+    return image;
+  }
+}
---
+++ lib/presentation/widgets/images/app_svg.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AppSvg extends StatelessWidget {
+  const AppSvg(
+    this.path, {
+    super.key,
+    this.width,
+    this.height,
+    this.color,
+    this.fit = BoxFit.contain,
+    this.placeholder,
+    this.errorWidget,
+  });
+
+  final String path;
+  final double? width;
+  final double? height;
+  final Color? color;
+  final BoxFit fit;
+  final Widget? placeholder;
+  final Widget? errorWidget;
+
+  bool get _isNetwork =>
+      path.startsWith('http://') || path.startsWith('https://');
+
+  @override
+  Widget build(BuildContext context) {
+    final colorFilter =
+        color != null ? ColorFilter.mode(color!, BlendMode.srcIn) : null;
+
+    if (_isNetwork) {
+      return SvgPicture.network(
+        path,
+        width: width,
+        height: height,
+        fit: fit,
+        colorFilter: colorFilter,
+        placeholderBuilder: (_) =>
+            placeholder ??
+            const Center(
+              child: SizedBox(
+                width: 16,
+                height: 16,
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+            ),
+        errorBuilder: (_, __, ___) =>
+            errorWidget ??
+            const Icon(
+              Icons.broken_image_outlined,
+              size: 20,
+            ),
+      );
+    }
+
+    return SvgPicture.asset(
+      path,
+      width: width,
+      height: height,
+      fit: fit,
+      colorFilter: colorFilter,
+    );
+  }
+}
