958694f5e6ee66655ed819111249921563cb18d6
djy <djybeyond@163.com>
2025-12-22 17:30:24 +0800

    add libs

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,9 +13,27 @@ class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            // SliverToBoxAdapter(child: CarouselSlider(items: items, options: options),),
+            // SliverPersistentHeader(delegate: null),
+            SliverPadding(
+              padding: EdgeInsets.all(10),
+              sliver: MasonryGridView.count(
+                crossAxisCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Placeholder();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
